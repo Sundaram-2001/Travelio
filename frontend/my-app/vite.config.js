7b0e4e6ae573672ffb79cfcada7b2b598b6aa60b
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2026-01-26 18:09:44 +0530

    minor changes

--- frontend/my-app/vite.config.js
+++ frontend/my-app/vite.config.js
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		// Add this block
+		allowedHosts: true, 
+		// Optional: Ensure host is 0.0.0.0 so it's accessible outside the container
+		host: true 
+	}
 });
