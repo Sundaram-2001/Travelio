f1bb1b8727d2c0a9a7f815932f385a25d1c23d5c
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2026-01-27 21:42:51 +0530

    updated dockerfile for prod build

--- frontend/my-app/svelte.config.js
+++ frontend/my-app/svelte.config.js
@@ -1,11 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+// Change this line:
+// import adapter from '@sveltejs/adapter-auto'; 
+
+// To this:
+import adapter from '@sveltejs/adapter-node'; 
+
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
