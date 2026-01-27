a32fb8cb83e5176e1dad2a8140361daf7a78c2bc
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2026-01-27 23:57:05 +0530

    changes in supabaseClient.js

--- frontend/my-app/src/lib/supabaseClient.js
+++ frontend/my-app/src/lib/supabaseClient.js
@@ -1,6 +1,7 @@
+import { env } from '$env/dynamic/public';
 import { createBrowserClient } from '@supabase/ssr';
 
-const PUBLIC_SUPABASE_URL = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-const PUBLIC_SUPABASE_ANON_KEY = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+const PUBLIC_SUPABASE_URL = env.PUBLIC_SUPABASE_URL || '';
+const PUBLIC_SUPABASE_ANON_KEY = env.PUBLIC_SUPABASE_ANON_KEY || ''
 
 export const supabase = createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
