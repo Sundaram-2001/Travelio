d45f229a4761ae559482f5c1b03e6381c2d9d25e
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2026-01-29 09:00:43 +0530

    minor changes in frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 import express from "express";
 import router from "./routes/routes.js";
+import cors from "cors"
+
 
 const app = express();
 
+
+app.use(cors({
+    origin:'https://travelio-1-hq8n.onrender.com',
+    credentials:true
+}))
 app.use(express.json());
 
 app.use("/", router); 
